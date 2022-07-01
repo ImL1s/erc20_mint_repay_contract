3946279bb3955da54daae4fe2cce86e7d69f3560
tw_sam <sam@wsp.com>
2022-07-01 10:15:39 +0700

    first commit

---
+++ mint_repay_token.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.6.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.6.0/contracts/access/Ownable.sol";
+
+contract USS is ERC20,Ownable {
+
+    ERC20 public usdt;
+
+    event MintUss(address user ,uint amount);
+    event RepayUss(address user ,uint amount);
+
+    constructor(address usdtAddress) ERC20("USS", "USS") {
+        usdt = ERC20(usdtAddress);
+    }
+
+    function getAllEther() public onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function mint(uint amount) public {
+        uint userUsdtBalance = usdt.balanceOf(msg.sender);
+        require(userUsdtBalance > 0, "You need get more USDT!");
+        uint256 allowance = usdt.allowance(msg.sender, address(this));
+        require(allowance >= amount, "Check the USDt allowance");
+        usdt.transferFrom(msg.sender, address(this), amount);
+        _mint(msg.sender, amount);
+        emit MintUss(msg.sender, amount);
+    }
+
+    function repay(uint amount) public {
+        uint userUssBalance = balanceOf(msg.sender);
+        require(userUssBalance > 0, "You need get more USS!");
+        require(userUssBalance >= amount, "You don't have enough USS!");
+        _burn(msg.sender, amount);
+        usdt.transfer(msg.sender, amount);
+        emit RepayUss(msg.sender, amount);
+    }
+}
